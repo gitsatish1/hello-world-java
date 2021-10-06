39ebee365840b8221e6bddfee21b426e482c4bcb
gitsatish1 <91123888+gitsatish1@users.noreply.github.com>
2021-10-06 19:13:07 +0530

    Update HelloWorld.java
    added new line from github

--- HelloWorld.java
+++ HelloWorld.java
@@ -37,5 +37,7 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hi");
     System.out.println("added newline from console");
+    System.out.println("added newline from console");
+    System.out.println("added newline from github");
   }
 }
